664e1f105929b65be2ca6d07c160a7238ec7a9be
Rudolfs Berzins <rudolfs.berzins.python@gmail.com>
2019-10-27 20:21:19 +0100

    Cloned part2 repository

---
+++ build/precache-manifest.2a460babfa811328ecffce226ff07753.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609feec40a9507f303ff0d9220b19771",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cab7a6854d890c7febc5",
+    "url": "/static/css/main.6a44819a.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "cab7a6854d890c7febc5",
+    "url": "/static/js/main.b82b9cef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
